1b4939c8536d785f90cb90e84512b16da87aca8d
Evan Hungate <hungate.evan@gmail.com>
2026-01-07 20:52:34 -0500

    Initial Commit

---
+++ YASA Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YASA Watch App
+//
+//  Created by Evan Hungate on 2026-01-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YASA Watch App/YASAApp.swift
@@ -0,0 +1,17 @@
+//
+//  YASAApp.swift
+//  YASA Watch App
+//
+//  Created by Evan Hungate on 2026-01-07.
+//
+
+import SwiftUI
+
+@main
+struct YASA_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
